1a0571df9ec169909120b48af2b5833d4ab7f81e
Diego Serrano <diegojserrano@gmail.com>
2023-10-09 11:11:39 -0300

    Clase 16

---
+++ clase16/ABMPersonas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+
+SqlConnection c = new SqlConnection("Server=localhost;Database=Personas;User Id=sa;Password=sa2k19..;");
+try
+{
+    c.Open();
+    
+}
+catch (System.Data.SqlClient.SqlException e)
+{
+    System.Console.WriteLine(c.ToString());
+    System.Console.WriteLine(e.Message);
+}
+System.Console.WriteLine(c.State);
+
+
+SqlCommand comm = c.CreateCommand();
+comm.CommandText = "select * from Personas";
+SqlDataReader dr = comm.ExecuteReader();
+int cant = 0;
+
+
+while(dr.Read()) { // Una vuelta por cada fila
+    int doc = dr.GetInt32("documento");
+    string nom = dr.GetString("nombre");
+    string ape = dr.GetString("apellido");
+    int edad = dr.GetInt32("edad");
+    cant++;
+    System.Console.WriteLine($"{doc} {nom} {ape} {edad}");
+}
+
+
+dr.Close();
+
+c.Close();
+
+
+System.Console.WriteLine($"Se leyeron {cant} personas");
---
+++ clase16/README.md
@@ -0,0 +1,14 @@
+## Clase 16
+
+* Acceso a bases de datos con .net
+* Paquetes System.Data y System.Data.SqlClient
+* Clases Connection, Command y ResultSet
+
+## Tarea
+
+* Crear un programa con interfaz de texto que presente un menú con opciones para insertar, consultar y borrar personas de la tabla de Personas.
+* Agregarle una opción para consultar una identificandola por documento.
+* Agregarle una opción para modificar los datos de una persona.
+* Agregarle una opción para listar los datos de las personas buscando por apellido.
+* Agregarle una opción que muestre cuántas personas hay por cada apellido.
+  
